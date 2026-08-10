291613d7586db1c2d13203bc54e78c824c3e9e7f
Christian Kirkegaard <christian@lowpoly.dk>
2026-08-11 01:07:38 +0200

    Initial Commit

---
+++ occuporto/occuportoApp.swift
@@ -0,0 +1,17 @@
+//
+//  occuportoApp.swift
+//  occuporto
+//
+//  Created by Christian Kirkegaard on 11/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct occuportoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
